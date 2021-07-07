782626a7420268f283452218c23ce0a1c75a72af
hyird <31695447+hyird@users.noreply.github.com>
2021-07-08 07:00:57 +0800

    Update custom.sh

--- user/lienol-x64-main/custom.sh
+++ user/lienol-x64-main/custom.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Test custom.sh"
+rm -rf package/lean/MTK7615-DBDC-LINUX5.4
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 
