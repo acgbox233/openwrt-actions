b893d2e4832450092f36e3d7d4f26579257ac1d8
hyird <31695447+hyird@users.noreply.github.com>
2021-07-10 06:37:40 +0800

    Update custom.sh

--- user/lienol-x64-main/custom.sh
+++ user/lienol-x64-main/custom.sh
@@ -5,4 +5,5 @@ rm -rf package/lean/MTK7615-DBDC-LINUX5.4
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-theme-argon.git       package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
 
